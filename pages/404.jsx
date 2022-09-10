9450bdeb415e50db12de3c7af9328d1339002ecd
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-11 00:33:25 +0100

    chore:typo

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function FourOhFour() {
   return (
     <>
-      <h1>404 - Page Not Found</h1>
+      <h1>404 | Page Not Found</h1>
       <Link href="/index">
         <a>Go back home</a>
       </Link>
