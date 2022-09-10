01969e3b90e7dbee8b0a97ba1a76d0e8ca2926bd
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-10 11:22:24 +0100

    style: added the style to the navbar

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,8 +1,15 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  background: #000;
+  height: 80px;
+  color: #fff;
+`;
 const Navbar = () => {
   return (
-    <div>
+    <Nav>
       <h1>Navbar</h1>
-    </div>
+    </Nav>
   );
 };
 
