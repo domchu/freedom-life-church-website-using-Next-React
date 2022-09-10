3612698f8c764414c51a770528e50dccf061a7c0
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-10 12:43:29 +0100

    text:added the footer texts

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
+const FooterSection = styled.div`
+  background: #000;
+  color: #fff;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const Footer = () => {
-  return <div>Footer</div>;
+  return <FooterSection>Next 2020 all right reserved</FooterSection>;
 };
 
 export default Footer;
