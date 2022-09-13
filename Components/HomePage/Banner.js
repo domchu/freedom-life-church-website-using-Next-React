274d631b477fff4a921a635c28681888b697b50e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-14 00:00:54 +0100

    text:added the texts from chakra ui

--- Components/HomePage/Banner.js
+++ Components/HomePage/Banner.js
@@ -4,7 +4,17 @@ import { Text } from "@chakra-ui/react";
 const Banner = () => {
   return (
     <div className="banner__container">
-      <Text fontSize="sm">Welcome to our church</Text>
+      <stack>
+        <Text fontSize="sm">Welcome to our church</Text>
+        <Text fontSize="6xl">
+          BECOME A PART OF <br></br>OUR COMMUNITY
+        </Text>
+      </stack>
+      <stack>
+        <Button colorScheme="yellow" size="md">
+          Button
+        </Button>
+      </stack>
     </div>
   );
 };
