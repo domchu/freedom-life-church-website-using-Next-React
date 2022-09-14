ae2ff70716f132846b030e830a0e0e6ed8a07a67
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-14 14:00:22 +0100

    style:styled the banner text

--- Components/HomePage/Banner.js
+++ Components/HomePage/Banner.js
@@ -11,19 +11,19 @@ const Banner = () => {
           <Text fontSize="sm" color="#fff">
             Welcome to our church
           </Text>
-          <Text fontSize="6xl" color="#fff">
+          <Text fontSize="6xl" color="#fff" fontWeight="600">
             BECOME A PART OF <br />
             OUR COMMUNITY
           </Text>
         </stack>
         <stack>
-          <Button colorScheme="yellow" size="md" p="7">
+          <Button colorScheme="yellow" size="md" p="7" m="2rem 0">
             LEARN MORE
           </Button>
         </stack>
         <div className={components.dash__text} color="#fff">
-          <BsDash color="#fff" />
-          <Text color="#fff">
+          <BsDash color="#fff" fontSize="2rem" />
+          <Text color="#fff" marginLeft="1rem">
             Lorem ipsum dolor si amet consectetur <br /> adipisicing elit.
             Iusto, eos!
           </Text>
