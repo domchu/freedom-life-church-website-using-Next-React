abf85eb15fb548a49257eaa6334e5f82a58cd64c
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-15 00:46:45 +0100

    style:style the footer logo and text

--- Components/container/Footer.jsx
+++ Components/container/Footer.jsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import Images from "../../public/Image";
 import components from "../components.module.css";
 import Link from "next/link";
+import { Text } from "@chakra-ui/react";
 
 const Section = {
   backgroundColor: "#000",
   color: "#fff",
-  height: "60px",
+  height: "50vh",
   display: " flex",
   justifyContent: "center",
   alignItems: "center",
@@ -21,8 +22,20 @@ const Footer = () => {
             <Image src={Images.logo} alt="" />
           </a>
         </Link>
+        <Text fontSize="sm" fontStyle="600" margin=".7rem 0 2rem 0">
+          &copy; FINSWEET 2022
+        </Text>
+        <Text fontSize="sm" fontStyle="600" margin=".5rem 0">
+          (480) 590 2316
+        </Text>
+        <Text fontSize="sm" fontStyle="600" margin=".5rem 0">
+          4517 WASHINGTON AVE.
+        </Text>
+        <Text fontSize="sm" fontStyle="600" margin=".5rem 0">
+          FINSWEET@EXAMPLE.COM
+        </Text>
       </div>
-      Copyright &copy; Next/React by Mr Dominic 2022 all right reserved
+      {/* Copyright &copy; Next/React by Mr Dominic 2022 all right reserved */}
     </footer>
   );
 };
