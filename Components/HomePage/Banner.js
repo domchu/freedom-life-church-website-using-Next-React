fb1b95c2d80133fb446201086a87e06a889923c3
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-14 13:16:38 +0100

    text:align banner text

--- Components/HomePage/Banner.js
+++ Components/HomePage/Banner.js
@@ -6,26 +6,28 @@ import components from "../components.module.css";
 const Banner = () => {
   return (
     <div className={components.banner__container}>
-      <stack style={banner__text}>
-        <Text fontSize="sm" color="#fff">
-          Welcome to our church
-        </Text>
-        <Text fontSize="6xl" color="#fff">
-          BECOME A PART OF <br />
-          OUR COMMUNITY
-        </Text>
-      </stack>
-      <stack>
-        <Button colorScheme="yellow" size="md" p="7">
-          LEARN MORE
-        </Button>
-      </stack>
-      <div className={components.dash__text} color="#fff">
-        <BsDash color="#fff" />
-        <Text color="#fff">
-          Lorem ipsum dolor si amet consectetur <br /> adipisicing elit. Iusto,
-          eos!
-        </Text>
+      <div className={components.banner__text}>
+        <stack>
+          <Text fontSize="sm" color="#fff">
+            Welcome to our church
+          </Text>
+          <Text fontSize="6xl" color="#fff">
+            BECOME A PART OF <br />
+            OUR COMMUNITY
+          </Text>
+        </stack>
+        <stack>
+          <Button colorScheme="yellow" size="md" p="7">
+            LEARN MORE
+          </Button>
+        </stack>
+        <div className={components.dash__text} color="#fff">
+          <BsDash color="#fff" />
+          <Text color="#fff">
+            Lorem ipsum dolor si amet consectetur <br /> adipisicing elit.
+            Iusto, eos!
+          </Text>
+        </div>
       </div>
     </div>
   );
