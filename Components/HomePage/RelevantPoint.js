7214ca076173492da7bf6d0369ddd2946afef664
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-16 00:19:22 +0100

    text:added text to the relevant point

--- Components/HomePage/RelevantPoint.js
+++ Components/HomePage/RelevantPoint.js
@@ -6,6 +6,12 @@ const RelevantPoint = () => {
   return (
     <div>
       <Image src={Images.icon1} alt="" />
+      <h2>ABOUT US</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore labore
+        harum consectetur doloribus rerum blanditiis qui mollitia recusandae ad
+        sequi!
+      </p>
     </div>
   );
 };
