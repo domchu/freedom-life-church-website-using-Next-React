0a476f17cebbdfb622412829310c05071a3a3343
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-15 15:06:48 +0100

    feature:import relevant point to the homepage

--- Components/HomePage/HomePage.jsx
+++ Components/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner";
 import ChurchRelevant from "./ChurchRelevant";
+import RelevantPoint from "./RelevantPoint";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
       <ChurchRelevant />
+      <RelevantPoint />
     </div>
   );
 };
---
+++ Components/HomePage/RelevantPoint.js
@@ -0,0 +1,7 @@
+import components from "./components.module.css";
+
+const RelevantPoint = () => {
+  return <div></div>;
+};
+
+export default RelevantPoint;
