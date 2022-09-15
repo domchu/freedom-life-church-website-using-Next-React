2b5efc585b8165699ade340fb40d26458d4c9e5e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-15 14:40:44 +0100

    text: passing the subheading as props

--- Components/HomePage/ChurchRelevant.js
+++ Components/HomePage/ChurchRelevant.js
@@ -3,7 +3,7 @@ import SubHeading from "../SubHeading";
 const ChurchRelevant = () => {
   return (
     <div>
-      <SubHeading />
+      <SubHeading pText="SUB-HEADLINE" hText="A CHURCH THAT'S RELEVANT" />
     </div>
   );
 };
--- Components/SubHeading.js
+++ Components/SubHeading.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Text } from "@chakra-ui/react";
 
-const SubHeading = () => {
-  return <div></div>;
+const SubHeading = (props) => {
+  return (
+    <div>
+      <Text fontSize="5x1">{props.pText}</Text>
+      <Text fontSize="6x1">{props.hText}</Text>
+    </div>
+  );
 };
 
 export default SubHeading;
