c0e5f941561b5eef206516eec89878280f7828ea
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-16 11:15:33 +0100

    text:added the love text

--- Components/HomePage/LoveCompassion.js
+++ Components/HomePage/LoveCompassion.js
@@ -2,9 +2,17 @@ import SubHeading from "../SubHeading";
 
 const LoveCompassion = () => {
   return (
-    <div>
-      <SubHeading pText="SUB-HEADLINE" hText="LOVE AND COMPASSION" />
-    </div>
+    <>
+      <div>
+        <SubHeading pText="SUB-HEADLINE" hText="LOVE AND COMPASSION" />
+      </div>
+      <p className={components.love__text}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+        consectetur eveniet ab voluptatibus amet illum natus, voluptate minima
+        beatae repellat molestias aperiam quas facilis aspernatur odio
+        laudantium! Distinctio, officia amet.
+      </p>
+    </>
   );
 };
 
