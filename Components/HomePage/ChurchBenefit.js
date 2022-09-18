f66b9500e03dbbd5e761862eb2c8df1c802ffa5f
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-18 23:23:57 +0100

    text:added the subheading

--- Components/HomePage/ChurchBenefit.js
+++ Components/HomePage/ChurchBenefit.js
@@ -1,11 +1,17 @@
 import components from "../components.module.css";
 import Image from "next/image";
 import Images from "../../public/Image";
+import SubHeading from "../SubHeading";
 
 const ChurchBenefit = () => {
   return (
     <div>
-      <h1>benefit</h1>
+      <div>
+        <SubHeading
+          pText="WATCH AND LISTEN"
+          hText="THE BENEFITS OF  JOINING OUR CHURCH"
+        />
+      </div>
     </div>
   );
 };
