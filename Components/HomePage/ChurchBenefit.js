3b26b1fccf0bc4be7d06d28ced02b96ebc46b657
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-18 23:19:48 +0100

    import images, styles,img to church benefits comp

--- Components/HomePage/ChurchBenefit.js
+++ Components/HomePage/ChurchBenefit.js
@@ -1,5 +1,13 @@
+import components from "../components.module.css";
+import Image from "next/image";
+import Images from "../../public/Image";
+
 const ChurchBenefit = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>benefit</h1>
+    </div>
+  );
 };
 
 export default ChurchBenefit;
