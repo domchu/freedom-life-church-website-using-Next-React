1809d1ad1838749575193e1d89a80a9802ff0ca9
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-19 23:16:09 +0100

    text:added the title

--- Components/HomePage/UpcomeSermon.js
+++ Components/HomePage/UpcomeSermon.js
@@ -5,7 +5,7 @@ const UpComeSermon = () => {
   return (
     <>
       <div>
-        <SubHeading pText="SUB-HEADLINE" hText="A CHURCH THAT'S RELEVANT" />
+        <SubHeading pText="UPCOMING SERMONS" hText="A CHURCH THAT'S RELEVANT" />
       </div>
       <Flex></Flex>
     </>
