43c8537504950cfd9f51edf0723f10c9921ce69f
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-20 22:52:06 +0100

    chore:import button from chakra

--- Components/HomePage/TheWorld.js
+++ Components/HomePage/TheWorld.js
@@ -1,4 +1,6 @@
 import components from "../components.module.css";
+import Link from "next/link";
+import { Button } from "@chakra-ui/react";
 
 const TheWorld = () => {
   return (
@@ -10,6 +12,11 @@ const TheWorld = () => {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
             tenetur.
           </p>
+          <Link href="/Sermon" passHref>
+            <Button bg="#000" size="md" p="6" m="1.5rem 0" color="#fff">
+              <a>REGISTER</a>
+            </Button>
+          </Link>
         </div>
       </div>
     </>
