b1ebf06b7c31102caa01a1f2bed8895c8a24a234
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-20 12:48:30 +0100

    style: style the bg color

--- Components/HomePage/UpcomeSermon.js
+++ Components/HomePage/UpcomeSermon.js
@@ -16,34 +16,36 @@ const UpComeSermon = () => {
           hText="JOIN US AND BECOME PART OF SOMETHING GREAT"
         />
       </div>
-      <Flex justifyContent="center" margin="3rem 0" width="100%" bg="#fff5eb">
-        <div className={components.sermon__text}>
-          <h4>Upcoming event</h4>
-          <h1 className={components.sermon__title}>
-            WATCH AND LISTEN <br /> TO OUR SERMONS
-          </h1>
+      <Flex justifyContent="center" margin="3rem 0" width="100%">
+        <div className={components.sermon__container}>
+          <div className={components.sermon__text}>
+            <h4>Upcoming event</h4>
+            <h1 className={components.sermon__title}>
+              WATCH AND LISTEN <br /> TO OUR SERMONS
+            </h1>
 
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident,
-            a sunt. Quisquam.
-          </p>
-          <div className={components.time}>
-            <FcClock />
-            <span>
-              Friday 23:39 IST <br /> sunday 11:20 ISD
-            </span>
+            <p style={{ marginBottom: 2 }}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Provident, a sunt. Quisquam.
+            </p>
+            <div className={components.time}>
+              <FcClock />
+              <span>
+                Friday 23:39 IST <br /> sunday 11:20 ISD
+              </span>
+            </div>
+            <div className={components.time}>
+              <FcSearch />
+              <span>
+                No 233 main st. New York, <br /> United state
+              </span>
+            </div>
+            <Link href="/sermon" passHref>
+              <Button bg="#000" size="md" p="7" m="2rem 0" color="#fff">
+                <a>REGISTER</a>
+              </Button>
+            </Link>
           </div>
-          <div className={components.time}>
-            <FcSearch />
-            <span>
-              No 233 main st. New York, <br /> United state
-            </span>
-          </div>
-          <Link href="/sermon" passHref>
-            <Button bg="#000" size="md" p="7" m="2rem 0" color="#fff">
-              <a>REGISTER</a>
-            </Button>
-          </Link>
         </div>
         <div className="sermon__img">
           <Image src={Images.sermonImage} alt="sermon image" />
