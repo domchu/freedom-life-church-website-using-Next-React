6031c31b52884d4e4ba5103bc9853d4f2761ac2b
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-20 22:48:36 +0100

    text:added the texts to the world component

--- Components/HomePage/TheWorld.js
+++ Components/HomePage/TheWorld.js
@@ -1,5 +1,19 @@
+import components from "../components.module.css";
+
 const TheWorld = () => {
-  return <div>TheWorld</div>;
+  return (
+    <>
+      <div className={components.world__container}>
+        <div className={components.world__text}>
+          <h2>WE WANT TO SERVE THE WORLD AROUND US</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
+            tenetur.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TheWorld;
