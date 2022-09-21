f07229a704e6c6a044d60a8779fce99b0cc47c19
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-21 09:34:14 +0100

    text:add the sub heading to the read and blog file

--- Components/HomePage/ReadBlog.js
+++ Components/HomePage/ReadBlog.js
@@ -1,9 +1,14 @@
 import components from "../components.module.css";
 import { Text } from "@chakra-ui/react";
+import SubHeading from "../SubHeading";
 
 const ReadBlog = () => {
   return (
     <>
+      <div>
+        <SubHeading pText="READ OUR BLOG" hText="SHARE, INSPIRE AND INNOVATE" />
+      </div>
+
       <div className={components.read__container}>
         <div className={components.read__text}>
           <Text className={components.world__title}>
