9c6fee346557e9162ac95cdf373bfb6aee5fc487
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-21 09:40:28 +0100

    text:added the title and date

--- Components/HomePage/ReadBlog.js
+++ Components/HomePage/ReadBlog.js
@@ -9,17 +9,21 @@ const ReadBlog = () => {
         <SubHeading pText="READ OUR BLOG" hText="SHARE, INSPIRE AND INNOVATE" />
       </div>
 
-      <div className={components.read__container}>
-        <div className={components.read__text}>
-          <Text className={components.world__title}>
-            WE WANT TO <br />
-            SERVE THE WORLD
-            <br /> AROUND US
-          </Text>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur <br /> adipisicing elit.
-            Eaque, tenetur.
+      <div className={components.sermon__container}>
+        <div className={components.sermon__text}>
+          <h4>Upcoming event</h4>
+          <h1 className={components.sermon__title}>
+            WATCH AND LISTEN <br /> TO OUR SERMONS
+          </h1>
+
+          <p margin="">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident,
+            a sunt. Quisquam.
           </p>
+
+          <Text>
+            By Mathew Johnson <br /> Tuesday 13 May, 2021
+          </Text>
         </div>
       </div>
     </>
