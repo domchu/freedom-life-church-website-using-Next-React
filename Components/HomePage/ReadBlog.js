7e1c44b166a1e87cabc73f5196a12b52b3a94d62
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-21 09:27:06 +0100

    text:added the texts to the read and blog comp

--- Components/HomePage/ReadBlog.js
+++ Components/HomePage/ReadBlog.js
@@ -1,5 +1,23 @@
+import components from "../components.module.css";
+
 const ReadBlog = () => {
-  return <div>ReadBlog</div>;
+  return (
+    <>
+      <div className={components.world__container}>
+        <div className={components.world__text}>
+          <Text className={components.world__title}>
+            WE WANT TO <br />
+            SERVE THE WORLD
+            <br /> AROUND US
+          </Text>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur <br /> adipisicing elit.
+            Eaque, tenetur.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ReadBlog;
