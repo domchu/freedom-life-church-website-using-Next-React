62e53e79453cd9c44ea8ed67a993eb9733270caa
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-23 13:41:59 +0100

    address

--- Components/ContactPage/Contact.js
+++ Components/ContactPage/Contact.js
@@ -1,10 +1,27 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import components from "../components.module.css";
 const Contact = () => {
   return (
     <>
       <Box className={components.contact__container}>
-        <h2>contact</h2>
+        <Box className={components.contact__text}>
+          <span>contact</span>
+          <Text>
+            GET IN TOUCH WITH <br /> OUR CHURCH
+          </Text>
+        </Box>
+      </Box>
+      <Box>
+        <span>Address</span>
+        <Text>
+          NH 234 PUBLIC SQUARE <br /> SAN FRANCISCO 65368
+        </Text>
+      </Box>
+      <Box>
+        <span>Contact Details</span>
+        <Text>
+          (480) 555-0103 <br /> FINSWEET@EXAMPLE.COM
+        </Text>
       </Box>
     </>
   );
