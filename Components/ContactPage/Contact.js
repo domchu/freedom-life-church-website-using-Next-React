3e10c6d0e1b8f977dd990cc9c945f0c3a2f2ce1a
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-23 13:49:29 +0100

    social media links

--- Components/ContactPage/Contact.js
+++ Components/ContactPage/Contact.js
@@ -1,5 +1,7 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Flex } from "@chakra-ui/react";
+import Link from "next/link";
 import components from "../components.module.css";
+import { BsFacebook, BsTwitter, BsLinkedin } from "react-icons/bs";
 const Contact = () => {
   return (
     <>
@@ -11,18 +13,40 @@ const Contact = () => {
           </Text>
         </Box>
       </Box>
-      <Box>
-        <span>Address</span>
-        <Text>
-          NH 234 PUBLIC SQUARE <br /> SAN FRANCISCO 65368
-        </Text>
-      </Box>
-      <Box>
-        <span>Contact Details</span>
-        <Text>
-          (480) 555-0103 <br /> FINSWEET@EXAMPLE.COM
-        </Text>
-      </Box>
+      <div className={components.address}>
+        <Box>
+          <span>Address</span>
+          <Text>
+            NH 234 PUBLIC SQUARE <br /> SAN FRANCISCO 65368
+          </Text>
+        </Box>
+        <Box>
+          <span>Contact Details</span>
+          <Text>
+            (480) 555-0103 <br /> FINSWEET@EXAMPLE.COM
+          </Text>
+        </Box>
+        <Box>
+          <span>Find us here</span>
+          <Flex style={{}}>
+            <Link href="/" passHref>
+              <a>
+                <BsFacebook color="#ffd2a4" />
+              </a>
+            </Link>
+            <Link href="/" passHref>
+              <a>
+                <BsTwitter color="#ffd2a4" />
+              </a>
+            </Link>
+            <Link href="/" passHref>
+              <a>
+                <BsLinkedin color="#ffd2a4" />
+              </a>
+            </Link>
+          </Flex>
+        </Box>
+      </div>
     </>
   );
 };
