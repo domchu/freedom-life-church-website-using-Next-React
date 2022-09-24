a5a2de046104c53e5cea7a3bab7fc0e7f25ebb84
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-24 22:58:43 +0100

    chore:import the up come

--- Components/SermonPage/SermonPage.jsx
+++ Components/SermonPage/SermonPage.jsx
@@ -1,16 +1,24 @@
+import Link from "next/link";
 import components from "../components.module.css";
-import { Text } from "@chakra-ui/react";
+import { Text, Flex, Button } from "@chakra-ui/react";
+import { FcSearch, FcClock } from "react-icons/fc";
+import Image from "next/image";
+import Images from "../../public/Images";
+import UpComeSermon from "../HomePage/UpComeSermon";
 
 const SermonPage = () => {
   return (
-    <div className={components.sermon__container}>
-      <div className={components.sermon__text}>
-        <span className={components.contact__text__title}>Sermon</span>
-        <Text fontSize="" fontWeight="">
-          TAKE PART IN OUR SERMON
-        </Text>
+    <>
+      <div className={components.sermon__container__page}>
+        <div className={components.sermon__text__sermon}>
+          <span className={components.contact__text__title}>Sermon</span>
+          <Text fontSize="" fontWeight="">
+            TAKE PART IN OUR SERMON
+          </Text>
+        </div>
       </div>
-    </div>
+      <UpComeSermon />
+    </>
   );
 };
 
