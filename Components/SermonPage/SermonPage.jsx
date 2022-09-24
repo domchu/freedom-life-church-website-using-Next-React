a8b0fdcf3290cd9c6cbfd58e5aea2eba83f55a6d
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-24 21:59:27 +0100

    sermon title and texts

--- Components/SermonPage/SermonPage.jsx
+++ Components/SermonPage/SermonPage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import components from "../components.module.css";
+import { Text } from "@chakra-ui/react";
 
 const SermonPage = () => {
-  return <div>SermonPage</div>;
+  return (
+    <div className={components.sermon__container}>
+      <div className={components.sermon__text}>
+        <span className={components.contact__text__title}>contact</span>
+        <Text fontSize="" fontWeight="">
+          GET IN TOUCH WITH <br /> OUR CHURCH
+        </Text>
+      </div>
+    </div>
+  );
 };
 
 export default SermonPage;
