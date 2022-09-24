6b7b90c8e643225bb581d71adc96cf2e3cbaac17
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-24 23:12:55 +0100

    style:styled the texts title

--- Components/SermonPage/SermonPage.jsx
+++ Components/SermonPage/SermonPage.jsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
+// import Link from "next/link";
 import components from "../components.module.css";
 import { Text, Flex, Button } from "@chakra-ui/react";
-import { FcSearch, FcClock } from "react-icons/fc";
-import Image from "next/image";
-import Images from "../../public/Images";
+// import { FcSearch, FcClock } from "react-icons/fc";
+// import Image from "next/image";
+// import Images from "../../public/Images";
 import UpComeSermon from "../HomePage/UpComeSermon";
 
 const SermonPage = () => {
@@ -11,8 +11,8 @@ const SermonPage = () => {
     <>
       <div className={components.sermon__container__page}>
         <div className={components.sermon__text__sermon}>
-          <span className={components.contact__text__title}>Sermon</span>
-          <Text fontSize="" fontWeight="">
+          <span className={components.sermon__text__title}>Sermon</span>
+          <Text fontSize="2.5rem" fontWeight="bold">
             TAKE PART IN OUR SERMON
           </Text>
         </div>
