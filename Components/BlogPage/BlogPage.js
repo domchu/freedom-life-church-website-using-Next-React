814c08af6047d730624914a0e9cef92d5a002eaf
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-26 15:17:30 +0100

    added the title and sub title

--- Components/BlogPage/BlogPage.js
+++ Components/BlogPage/BlogPage.js
@@ -1,11 +1,18 @@
 import components from "../components.module.css";
+import { Box, Text } from "@chakra-ui/react";
+// import SubHeading from "../SubHeading";
 
 const BlogPage = () => {
   return (
     <>
-      <div className={components.blog__container}>
-        <h2>blogPage</h2>
-      </div>
+      <Box className={components.blog__container}>
+        <Box className={components.blog__text}>
+          <span className={components.blog__text__text}>OUR BLOG</span>
+          <Text fontSize="2.5rem" fontWeight="bold">
+            MOST RECENT POST
+          </Text>
+        </Box>
+      </Box>
     </>
   );
 };
